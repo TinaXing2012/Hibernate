cee960cd6e19aa9d84e5671805c97a8215dbde38
rxing <rxing@mum.edu>
2019-11-29 20:11:46 -0600

    Association Mapping - ManytoOne JoinTable

--- hibernatedemo3/src/main/java/edu/mum/cs/manytoone/Address.java
+++ hibernatedemo3/src/main/java/edu/mum/cs/manytoone/Address.java
@@ -3,10 +3,7 @@ package edu.mum.cs.manytoone;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Setter
 @Getter
@@ -19,7 +16,8 @@ public class Address {
     private String zipCode;
 
     @ManyToOne
-    @JoinColumn(name="person_id")
+//    @JoinColumn(name="person_id")
+    @JoinTable(name = "person_address", joinColumns = {@JoinColumn(name = "person_id")}, inverseJoinColumns = {@JoinColumn(name = "address_id")})
     private Person person;
 
 }
